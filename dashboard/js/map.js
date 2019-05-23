4ee538557e07a65ca96654a3b074c18e04ad9490
karmsidhu <karmveer.s.sidhu@outlook.com>
2019-05-22 20:23:42 -0600

    Added Map Style to CSS

--- dashboard/js/map.js
+++ dashboard/js/map.js
@@ -1,34 +1,21 @@
 const 
     width = 1000,
-		height = 600;
-		
-const svg = d3.select('#map')
-	.append('svg')
-	.attr('width', width)
-	.attr('height', height);
+		height = 600,
+		USATOPO = './lib/us-10m.v1.json',
+		svg = d3.select('#map')
+			.append('svg')
+			.attr('width', width)
+			.attr('height', height);
 
 var usaMap = svg
-	.append("g")
-	.attr("id", "mapGroup");
-
-const USATOPO = "https://d3js.org/us-10m.v1.json";
+	.append('g');
 
 d3.json(USATOPO)
 	.then(usa => {
-
-		usaMap.selectAll("path")
+		usaMap.selectAll('path')
 			.data(topojson.feature(usa, usa.objects.states).features)
-			.enter().append("path")
-			.attr("d", d3.geoPath())
-			.style('fill', 'lightgray')
-			.style('stroke', 'white')
-			.on('mouseover', function(d) {
-				d3.select(this)
-					.style('fill', 'rgb(255, 220, 149)');
-			})
-			.on('mouseout', function(d) {
-				d3.select(this)
-					.style('fill', 'lightgray');
-			});
+			.enter().append('path')
+			.attr('d', d3.geoPath())
+			.attr('class', 'usaMap');
 	})
 	.catch(exception => {console.log(exception);});
